a07fa75b850b199b5ad9de617769b4b2282b27d1
noorahmad01 <noor-sengg@hotmail.com>
2013-12-16 18:36:47 +0500

    First Checked In Project

---
+++ Mojosol/App_Start/AuthConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Membership.OpenAuth;
+
+namespace Mojosol
+{
+    internal static class AuthConfig
+    {
+        public static void RegisterOpenAuth()
+        {
+            // See http://go.microsoft.com/fwlink/?LinkId=252803 for details on setting up this ASP.NET
+            // application to support logging in via external services.
+
+            OpenAuth.AuthenticationClients.AddTwitter(
+                consumerKey: "KztTs2PVdJk2k5W2afS6w",
+                consumerSecret: "vo7YwAqYxPVkOzW1xnkd2ZUvu3rFEWkbj9gWG5FHbsE");
+
+            OpenAuth.AuthenticationClients.AddFacebook(
+                appId: "404440493022558",
+                appSecret: "05bf4777a1ff432c76658d96e53e88dd");
+
+            OpenAuth.AuthenticationClients.AddMicrosoft(
+                clientId: "000000004410C5A3",
+                clientSecret: "ByoP7kqChjIZrPAnA1rDcSZjIc5qANSh");
+
+            OpenAuth.AuthenticationClients.AddGoogle();
+        }
+    }
+}
---
+++ Mojosol/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mojosol.Global" Language="C#" %>
